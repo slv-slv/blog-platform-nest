f289cabfbc9d2dec3aeefab93fcd20d5ade152c9
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-03-20 23:23:29 +0300

    moved port to settings

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { SETTINGS } from './settings.js';
 
 const app = await NestFactory.create(AppModule);
 
-await app.listen(process.env.PORT ?? 3000);
+await app.listen(SETTINGS.PORT);
