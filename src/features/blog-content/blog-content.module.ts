904b2396b10f0ee0dfdef62a706e87368f600e3c
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-03-22 17:30:27 +0300

    added exports

--- src/features/blog-content/blog-content.module.ts
+++ src/features/blog-content/blog-content.module.ts
@@ -5,10 +5,22 @@ import { BlogsRepository } from './blogs/blogs.repository.js';
 import { BlogsQueryRepository } from './blogs/blogs.query-repository.js';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Blog, BlogSchema } from './blogs/blogs.schema.js';
+import { PostsController } from './posts/posts.controller.js';
+import { PostsService } from './posts/posts.service.js';
+import { PostsRepository } from './posts/posts.repository.js';
+import { PostsQueryRepository } from './posts/posts.query-repository.js';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: Blog.name, schema: BlogSchema }])],
   controllers: [BlogsController],
-  providers: [BlogsService, BlogsRepository, BlogsQueryRepository],
+  providers: [
+    BlogsService,
+    BlogsRepository,
+    BlogsQueryRepository,
+    // PostsService,
+    // PostsRepository,
+    // PostsQueryRepository,
+  ],
+  exports: [BlogsService, BlogsRepository, BlogsQueryRepository], // Экспортируем BlogsService
 })
 export class BlogContentModule {}
