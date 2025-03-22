49a619af0f6224b62397ded679f89fbc03bd3143
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-03-22 17:31:07 +0300

    added ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { BlogsController } from './features/blog-content/blogs/blogs.controller.js';
 import { SETTINGS } from './settings.js';
 import { BlogContentModule } from './features/blog-content/blog-content.module.js';
+import { PostsController } from './features/blog-content/posts/posts.controller.js';
 
 @Module({
-  imports: [MongooseModule.forRoot(SETTINGS.MONGO_URL), BlogContentModule],
-  controllers: [AppController, BlogsController],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(SETTINGS.MONGO_URL), BlogContentModule],
+  controllers: [AppController, BlogsController, PostsController],
   providers: [AppService],
 })
 export class AppModule {}
