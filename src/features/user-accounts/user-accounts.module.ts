a88167010572951c2669716f36f8f700fcfc5cf8
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-03-24 22:00:29 +0300

    added user accounts module

---
+++ src/features/user-accounts/user-accounts.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users/users.controller.js';
+import { UsersService } from './users/users.service.js';
+import { UsersRepository } from './users/users.repository.js';
+import { UsersQueryRepository } from './users/users.query-repository.js';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './users/users.schema.js';
+import { AuthController } from './auth/auth.controller.js';
+import { AuthService } from './auth/auth.service.js';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
+  controllers: [UsersController, AuthController],
+  providers: [UsersService, UsersRepository, UsersQueryRepository, AuthService],
+  exports: [UsersService, UsersRepository, UsersQueryRepository],
+})
+export class UserAccountsModule {}
