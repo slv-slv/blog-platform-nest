a56bab0a2c4340588f2a7b3a176d5bdc448ae9fd
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-03-24 22:01:37 +0300

    added paging params middleware for 3 routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,31 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
-import { BlogsController } from './features/blog-content/blogs/blogs.controller.js';
 import { SETTINGS } from './settings.js';
 import { BlogContentModule } from './features/blog-content/blog-content.module.js';
-import { PostsController } from './features/blog-content/posts/posts.controller.js';
+import { PagingMiddleware } from './common/middlewares/paging.middleware.js';
+import { UserAccountsModule } from './features/user-accounts/user-accounts.module.js';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(SETTINGS.MONGO_URL), BlogContentModule],
-  controllers: [AppController, BlogsController, PostsController],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(SETTINGS.MONGO_URL),
+    BlogContentModule,
+    UserAccountsModule,
+  ],
+  controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(PagingMiddleware)
+      .forRoutes(
+        { path: 'blogs', method: RequestMethod.GET },
+        { path: 'posts', method: RequestMethod.GET },
+        { path: 'users', method: RequestMethod.GET },
+      );
+  }
+}
