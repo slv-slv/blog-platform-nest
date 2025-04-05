43cd1ada8ce7620c079a2ec226842db8901b8bab
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-04-05 17:15:11 +0300

    enabled CORS and global transform pipe

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { SETTINGS } from './settings.js';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(SETTINGS.PORT);
 }
 
