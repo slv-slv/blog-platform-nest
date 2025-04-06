5b3e0f53e54ad46d2dab2c9666a5a40971d9c3c0
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-04-06 23:30:02 +0300

    added basic paging params class

--- src/common/types/paging-params.types.ts
+++ src/common/types/paging-params.types.ts
@@ -1,6 +1,17 @@
-export type PagingParamsType = {
-  sortBy: string;
-  sortDirection: 'asc' | 'desc';
-  pageNumber: number;
-  pageSize: number;
-};
+import { IsEnum, IsNumberString } from 'class-validator';
+
+export enum SortDirection {
+  asc = 'asc',
+  desc = 'desc',
+}
+
+export class BasicPagingParams {
+  @IsEnum(SortDirection)
+  sortDirection: SortDirection = SortDirection.desc;
+
+  @IsNumberString()
+  pageNumber: number = 1;
+
+  @IsNumberString()
+  pageSize: number = 10;
+}
