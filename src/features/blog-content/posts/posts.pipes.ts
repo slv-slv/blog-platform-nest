f9228c71e8fdc678fa14c46117b26aa2c7b9fcb8
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-04-08 00:22:04 +0300

    added GetPostsQueryPipe

---
+++ src/features/blog-content/posts/posts.pipes.ts
@@ -0,0 +1,14 @@
+import { ArgumentMetadata, BadRequestException, Injectable, PipeTransform } from '@nestjs/common';
+import { PostsSortBy } from './posts.types.js';
+
+@Injectable()
+export class GetPostsQueryPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    value.sortBy ??= PostsSortBy.createdAt;
+    if (!Object.values(PostsSortBy).includes(value.sortBy)) {
+      throw new BadRequestException('Invalid sortBy value');
+    }
+
+    return value;
+  }
+}
