3fb029bffeb4cc8f0b59e933f658427b983c2b56
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-04-13 23:02:41 +0300

    added basic auth guard

---
+++ src/common/guards/basic-auth.ts
@@ -0,0 +1,24 @@
+import { Observable } from '.store/rxjs-npm-7.8.2-80ecda9013/package';
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { SETTINGS } from '../../settings.js';
+
+@Injectable()
+export class BasicAuthGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    const req = context.switchToHttp().getRequest();
+
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) throw new UnauthorizedException('Authorization header missing');
+
+    const [authMethod, credsBase64] = authHeader.split(' ');
+    const credentials = SETTINGS.CREDENTIALS;
+
+    if (authMethod !== 'Basic') throw new UnauthorizedException('Invalid authorization method');
+
+    if (!credentials.map((user) => user.base64).includes(credsBase64))
+      throw new UnauthorizedException('Incorrect credentials');
+
+    return true;
+  }
+}
