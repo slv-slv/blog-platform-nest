28f6ea3537fbfa1b7bf8874237d21a658012a767
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-04-13 22:26:04 +0300

    moved setup to another module

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { SETTINGS } from './settings.js';
 import { ValidationPipe } from '@nestjs/common';
+import { appSetup } from './setup/app.setup.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
-  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true, forbidNonWhitelisted: true }));
+  appSetup(app);
 
   await app.listen(SETTINGS.PORT);
 }
