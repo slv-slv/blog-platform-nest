e6b218f09df1067899a669e1ed12ae914f43df8f
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-04-19 22:47:18 +0300

    added custom exception factory

--- src/setup/pipes.setup.ts
+++ src/setup/pipes.setup.ts
@@ -1,5 +1,19 @@
-import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { BadRequestException, INestApplication, ValidationError, ValidationPipe } from '@nestjs/common';
 
 export function pipesSetup(app: INestApplication) {
-  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true, forbidNonWhitelisted: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      stopAtFirstError: true,
+      exceptionFactory: (errors: ValidationError[]) => {
+        const errorMessages = errors.map((error) => ({
+          field: error.property,
+          message: Object.values(error.constraints!)[0],
+        }));
+        throw new BadRequestException({ errorMessages });
+      },
+    }),
+  );
 }
