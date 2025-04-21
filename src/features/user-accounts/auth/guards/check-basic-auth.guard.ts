b59d36c12149b54bd72bb7b43bedb9f77e11f523
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-04-21 16:54:18 +0300

    renamed basic auth guard

--- src/features/user-accounts/auth/guards/check-basic-auth.guard.ts
+++ src/features/user-accounts/auth/guards/check-basic-auth.guard.ts
@@ -1,9 +1,9 @@
 import { Observable } from '.store/rxjs-npm-7.8.2-80ecda9013/package';
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
-import { SETTINGS } from '../../settings.js';
+import { SETTINGS } from '../../../../settings.js';
 
 @Injectable()
-export class BasicAuthGuard implements CanActivate {
+export class CheckBasicAuth implements CanActivate {
   canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
     const req = context.switchToHttp().getRequest();
 
--- src/features/user-accounts/users/users.controller.ts
+++ src/features/user-accounts/users/users.controller.ts
@@ -15,10 +15,10 @@ import {
 import { UsersService } from './users.service.js';
 import { UsersQueryRepository } from './users.query-repository.js';
 import { CreateUserInputDto, GetUsersQueryParams, UsersPaginatedType, UserType } from './users.types.js';
-import { BasicAuthGuard } from '../../../common/guards/basic-auth.js';
+import { CheckBasicAuth } from '../auth/guards/check-basic-auth.guard.js';
 
 @Controller('users')
-@UseGuards(BasicAuthGuard)
+@UseGuards(CheckBasicAuth)
 export class UsersController {
   constructor(
     private readonly usersService: UsersService,
