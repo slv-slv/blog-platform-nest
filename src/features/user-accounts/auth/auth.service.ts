2009f2d671adac8ba038ee19e99b07deb3fa6b55
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-04-23 21:49:07 +0300

    replaced bcrypt with bcryptjs

--- src/features/user-accounts/auth/auth.service.ts
+++ src/features/user-accounts/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { UsersQueryRepository } from '../users/users.query-repository.js';
 import { JwtAcessPayload, JwtPairType, JwtRefreshPayload } from './auth.types.js';
 import { SETTINGS } from '../../../settings.js';
@@ -40,24 +40,4 @@ export class AuthService {
     });
     return refreshToken;
   }
-
-  // async generateJwtPair(userId: string, deviceId: string): Promise<JwtPairType> {
-  //   const jwtAccessPayload = { sub: userId };
-  //   const jwtRefreshPayload = { sub: userId, deviceId };
-
-  //   const accessToken = this.jwtService.sign(jwtAccessPayload, { expiresIn: SETTINGS.ACCESS_TOKEN_LIFETIME });
-  //   const refreshToken = this.jwtService.sign(jwtRefreshPayload, {
-  //     expiresIn: SETTINGS.REFRESH_TOKEN_LIFETIME,
-  //   });
-
-  //   return { accessToken, refreshToken };
-  // }
-
-  // verifyJwt(token: string): JwtAcessPayload | JwtRefreshPayload | null {
-  //   try {
-  //     return this.jwtService.verify(token) as JwtAcessPayload | JwtRefreshPayload;
-  //   } catch {
-  //     return null;
-  //   }
-  // }
 }
