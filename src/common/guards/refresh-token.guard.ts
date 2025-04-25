a43d972851386a0acb9f595d94372c78fa71608b
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-04-26 01:02:33 +0300

    added refresh token guard

---
+++ src/common/guards/refresh-token.guard.ts
@@ -0,0 +1,42 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request, Response } from 'express';
+import { JwtRefreshPayload } from '../../features/user-accounts/auth/auth.types.js';
+import { SessionsService } from '../../features/user-accounts/sessions/sessions.service.js';
+
+@Injectable()
+export class RefreshTokenGuard implements CanActivate {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly sessionsService: SessionsService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req: Request = context.switchToHttp().getRequest();
+    const res: Response = context.switchToHttp().getResponse();
+
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+      throw new UnauthorizedException('Invalid authorization method');
+    }
+
+    let payload: JwtRefreshPayload;
+
+    try {
+      payload = await this.jwtService.verifyAsync(refreshToken);
+    } catch {
+      throw new UnauthorizedException('Invalid access token');
+    }
+
+    const { sub, deviceId, iat } = payload;
+
+    const isSessionActive = this.sessionsService.checkSession(sub, deviceId, iat);
+    if (!isSessionActive) {
+      throw new UnauthorizedException('No active session found');
+    }
+
+    res.locals.userId = sub;
+    res.locals.deviceId = deviceId;
+    return true;
+  }
+}
