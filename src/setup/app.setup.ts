1e8417f7abbce9cbdd3ea7869ca1350f3c4fd0a0
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-04-26 01:10:35 +0300

    enabled cookie parser

--- src/setup/app.setup.ts
+++ src/setup/app.setup.ts
@@ -1,7 +1,9 @@
 import { INestApplication } from '@nestjs/common';
 import { pipesSetup } from './pipes.setup.js';
+import cookieParser from 'cookie-parser';
 
 export function appSetup(app: INestApplication) {
   app.enableCors();
+  app.use(cookieParser());
   pipesSetup(app);
 }
