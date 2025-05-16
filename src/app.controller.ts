57a00a9f5151848bb55159826a56bc9365b4639c
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-05-16 22:01:17 +0300

    changed dropDb method to raw sql

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,16 @@
 import mongoose from 'mongoose';
-import { Controller, Delete, Get, HttpCode } from '@nestjs/common';
+import { Controller, Delete, Get, HttpCode, Inject } from '@nestjs/common';
 import { AppService } from './app.service.js';
 import { InjectConnection } from '@nestjs/mongoose';
+import { pool } from './common/constants.js';
+import { Pool } from 'pg';
 
 @Controller()
 export class AppController {
   constructor(
     @InjectConnection() private readonly connection: mongoose.Connection,
     private readonly appService: AppService,
+    @Inject(pool) private readonly pool: Pool,
   ) {}
 
   @Get()
@@ -15,9 +18,17 @@ export class AppController {
     return this.appService.getHello();
   }
 
+  // @Delete('testing/all-data')
+  // @HttpCode(204)
+  // async dropDb(): Promise<void> {
+  //   await this.connection.dropDatabase();
+  // }
+
   @Delete('testing/all-data')
   @HttpCode(204)
   async dropDb(): Promise<void> {
-    await this.connection.dropDatabase();
+    await this.pool.query(`
+      TRUNCATE users, confirmation, recovery RESTART IDENTITY CASCADE
+    `);
   }
 }
