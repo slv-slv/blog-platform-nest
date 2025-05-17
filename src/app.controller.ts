382a907fdd9ce18681dd8c461ba242532cdbf912
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-05-17 23:54:10 +0300

    updated tables list in dropDb method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -28,7 +28,7 @@ export class AppController {
   @HttpCode(204)
   async dropDb(): Promise<void> {
     await this.pool.query(`
-      TRUNCATE users, confirmation, recovery RESTART IDENTITY CASCADE
+      TRUNCATE users, confirmation, recovery, devices RESTART IDENTITY CASCADE
     `);
   }
 }
