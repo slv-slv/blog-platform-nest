5ff79e65d8eba4bf6d945a97571b1e46cbda77b3
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-05-18 12:29:39 +0300

    changed smtp port

--- src/notifications/email/nodemailer.ts
+++ src/notifications/email/nodemailer.ts
@@ -2,10 +2,10 @@ import nodemailer from 'nodemailer';
 import { SETTINGS } from '../../settings.js';
 
 export const emailTransport = nodemailer.createTransport({
-  // host: 'smtp.gmail.com',
-  host: 'smtp.yandex.ru',
-  port: 587,
-  secure: false,
+  host: 'smtp.gmail.com',
+  // host: 'smtp.yandex.ru',
+  port: 465,
+  secure: true,
   auth: {
     user: SETTINGS.EMAIL_CREDENTIALS.user,
     pass: SETTINGS.EMAIL_CREDENTIALS.password,
