e50ad46d840f8d9066b74842e09c072552d0a875
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-05-19 15:48:23 +0300

    added getPosts sql method

--- src/features/blog-content/posts/infrastructure/mongoose/posts.query-repository.ts
+++ src/features/blog-content/posts/infrastructure/mongoose/posts.query-repository.ts
@@ -70,6 +70,49 @@ export class PostsQueryRepository {
     return { ...post, extendedLikesInfo };
   }
 
+  // async getPosts(
+  //   userId: string,
+  //   pagingParams: PagingParamsType,
+  //   blogId?: string,
+  // ): Promise<PostsPaginatedType> {
+  //   const { sortBy, sortDirection, pageNumber, pageSize } = pagingParams;
+
+  //   const filter = blogId ? { blogId } : {};
+
+  //   const totalCount = await this.model.countDocuments(filter);
+  //   const pagesCount = Math.ceil(totalCount / pageSize);
+
+  //   const postsWithObjectId = await this.model
+  //     .find(filter)
+  //     .sort({ [sortBy]: sortDirection === 'asc' ? 1 : -1 })
+  //     .skip((pageNumber - 1) * pageSize)
+  //     .limit(pageSize)
+  //     .lean();
+
+  //   const posts = await Promise.all(
+  //     postsWithObjectId.map(async (post) => {
+  //       return {
+  //         id: post._id.toString(),
+  //         title: post.title,
+  //         shortDescription: post.shortDescription,
+  //         content: post.content,
+  //         blogId: post.blogId,
+  //         blogName: post.blogName,
+  //         createdAt: post.createdAt,
+  //         extendedLikesInfo: await this.postLikesQueryRepository.getLikesInfo(post._id.toString(), userId),
+  //       };
+  //     }),
+  //   );
+
+  //   return {
+  //     pagesCount,
+  //     page: pageNumber,
+  //     pageSize,
+  //     totalCount,
+  //     items: posts,
+  //   };
+  // }
+
   async getPosts(
     userId: string,
     pagingParams: PagingParamsType,
@@ -77,31 +120,71 @@ export class PostsQueryRepository {
   ): Promise<PostsPaginatedType> {
     const { sortBy, sortDirection, pageNumber, pageSize } = pagingParams;
 
-    const filter = blogId ? { blogId } : {};
+    let orderBy: string;
 
-    const totalCount = await this.model.countDocuments(filter);
+    switch (sortBy) {
+      case 'shortDescription':
+        orderBy = 'short_description';
+        break;
+      case 'blogId':
+        orderBy = 'blog_id';
+        break;
+      case 'blogName':
+        orderBy = 'blogs.name';
+        break;
+      case 'createdAt':
+        orderBy = 'created_at';
+        break;
+      default:
+        orderBy = sortBy;
+    }
+
+    const whereClause = blogId ? `WHERE posts.blog_id = ${parseInt(blogId)}` : ``;
+
+    const countResult = await this.pool.query(
+      `
+        SELECT id
+        FROM posts
+        ${whereClause}
+      `,
+    );
+
+    const totalCount = countResult.rowCount!;
     const pagesCount = Math.ceil(totalCount / pageSize);
+    const skipCount = (pageNumber - 1) * pageSize;
 
-    const postsWithObjectId = await this.model
-      .find(filter)
-      .sort({ [sortBy]: sortDirection === 'asc' ? 1 : -1 })
-      .skip((pageNumber - 1) * pageSize)
-      .limit(pageSize)
-      .lean();
+    const postsResult = await this.pool.query(
+      `
+        SELECT
+          posts.id,
+          posts.title,
+          posts.short_description,
+          posts.content,
+          posts.blog_id,
+          blogs.name AS blog_name,
+          posts.created_at
+        FROM posts JOIN blogs ON posts.blog_id = blogs.id
+        ${whereClause}
+        ORDER BY ${orderBy} ${sortDirection}
+        LIMIT $1
+        OFFSET $2
+      `,
+      [pageSize, skipCount],
+    );
+
+    const rawPosts = postsResult.rows;
 
     const posts = await Promise.all(
-      postsWithObjectId.map(async (post) => {
-        return {
-          id: post._id.toString(),
-          title: post.title,
-          shortDescription: post.shortDescription,
-          content: post.content,
-          blogId: post.blogId,
-          blogName: post.blogName,
-          createdAt: post.createdAt,
-          extendedLikesInfo: await this.postLikesQueryRepository.getLikesInfo(post._id.toString(), userId),
-        };
-      }),
+      rawPosts.map(async (post) => ({
+        id: post.id.toString(),
+        title: post.title,
+        shortDescription: post.short_description,
+        content: post.content,
+        blogId: post.blog_id,
+        blogName: post.blog_name,
+        createdAt: post.created_at,
+        extendedLikesInfo: await this.postLikesQueryRepository.getLikesInfo(post._id.toString(), userId),
+      })),
     );
 
     return {
