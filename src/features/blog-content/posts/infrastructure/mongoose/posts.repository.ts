0d938334af04d57500ba64afa7ae2304da42ae28
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-05-19 15:53:11 +0300

    added findPost sql method

--- src/features/blog-content/posts/infrastructure/mongoose/posts.repository.ts
+++ src/features/blog-content/posts/infrastructure/mongoose/posts.repository.ts
@@ -1,28 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Post } from './posts.schemas.js';
 import { ObjectId } from 'mongodb';
 import { PostDtoType } from '../../posts.types.js';
 import { BlogsRepository } from '../../../blogs/infrastructure/mongoose/blogs.repository.js';
+import { pool } from '../../../../../common/constants.js';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PostsRepository {
   constructor(
     @InjectModel(Post.name) private readonly model: Model<Post>,
+    @Inject(pool) private readonly pool: Pool,
     private readonly blogsRepository: BlogsRepository,
   ) {}
 
+  // async findPost(id: string): Promise<PostDtoType | null> {
+  //   if (!ObjectId.isValid(id)) {
+  //     return null;
+  //   }
+  //   const _id = new ObjectId(id);
+  //   const post = await this.model.findOne({ _id }, { _id: 0 }).lean();
+  //   if (!post) {
+  //     return null;
+  //   }
+  //   return { id, ...post };
+  // }
+
   async findPost(id: string): Promise<PostDtoType | null> {
-    if (!ObjectId.isValid(id)) {
+    const result = await this.pool.query(
+      `
+        SELECT
+          posts.title,
+          posts.short_description,
+          posts.content,
+          posts.blog_id,
+          blogs.name AS blog_name,
+          posts.created_at,
+        FROM posts JOIN blogs
+          ON posts.blog_id = blogs.id
+        WHERE posts.id = $1
+      `,
+      [parseInt(id)],
+    );
+
+    if (result.rowCount === 0) {
       return null;
     }
-    const _id = new ObjectId(id);
-    const post = await this.model.findOne({ _id }, { _id: 0 }).lean();
-    if (!post) {
-      return null;
-    }
-    return { id, ...post };
+
+    const rawPost = result.rows[0];
+
+    return {
+      id,
+      title: rawPost.title,
+      shortDescription: rawPost.short_description,
+      content: rawPost.content,
+      blogId: rawPost.blog_id.toString(),
+      blogName: rawPost.blog_name,
+      createdAt: rawPost.created_at,
+    };
   }
 
   async createPost(
