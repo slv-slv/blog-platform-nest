2dfa0382b024fe68766020ec9d9ff1d922292855
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-05-19 15:08:02 +0300

    added findPost sql method

--- src/features/blog-content/posts/infrastructure/mongoose/posts.query-repository.ts
+++ src/features/blog-content/posts/infrastructure/mongoose/posts.query-repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Post } from './posts.schemas.js';
@@ -6,27 +6,68 @@ import { PostsPaginatedType, PostViewType } from '../../posts.types.js';
 import { ObjectId } from 'mongodb';
 import { PagingParamsType } from '../../../../../common/types/paging-params.types.js';
 import { PostLikesQueryRepository } from '../../../likes/posts/infrastructure/mongoose/post-likes.query-repository.js';
+import { pool } from '../../../../../common/constants.js';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PostsQueryRepository {
   constructor(
     @InjectModel(Post.name) private readonly model: Model<Post>,
+    @Inject(pool) private readonly pool: Pool,
     private readonly postLikesQueryRepository: PostLikesQueryRepository,
   ) {}
 
+  // async findPost(id: string, userId: string): Promise<PostViewType | null> {
+  //   if (!ObjectId.isValid(id)) {
+  //     return null;
+  //   }
+  //   const _id = new ObjectId(id);
+  //   const post = await this.model.findOne({ _id }, { _id: 0 }).lean();
+  //   if (!post) {
+  //     return null;
+  //   }
+
+  //   const extendedLikesInfo = await this.postLikesQueryRepository.getLikesInfo(id, userId);
+
+  //   return { id, ...post, extendedLikesInfo };
+  // }
+
   async findPost(id: string, userId: string): Promise<PostViewType | null> {
-    if (!ObjectId.isValid(id)) {
+    const result = await this.pool.query(
+      `
+        SELECT
+          posts.title,
+          posts.short_description,
+          posts.content,
+          posts.blog_id,
+          blogs.name AS blog_name,
+          posts.created_at,
+        FROM posts JOIN blogs
+          ON posts.blog_id = blogs.id
+        WHERE posts.id = $1
+      `,
+      [parseInt(id)],
+    );
+
+    if (result.rowCount === 0) {
       return null;
     }
-    const _id = new ObjectId(id);
-    const post = await this.model.findOne({ _id }, { _id: 0 }).lean();
-    if (!post) {
-      return null;
-    }
+
+    const rawPost = result.rows[0];
+
+    const post = {
+      id,
+      title: rawPost.title,
+      shortDescription: rawPost.short_description,
+      content: rawPost.content,
+      blogId: rawPost.blog_id.toString(),
+      blogName: rawPost.blog_name,
+      createdAt: rawPost.created_at,
+    };
 
     const extendedLikesInfo = await this.postLikesQueryRepository.getLikesInfo(id, userId);
 
-    return { id, ...post, extendedLikesInfo };
+    return { ...post, extendedLikesInfo };
   }
 
   async getPosts(
