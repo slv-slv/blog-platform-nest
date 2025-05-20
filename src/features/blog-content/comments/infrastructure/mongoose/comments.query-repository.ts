012b1ec052d376e9ff2a1d859c54f7c09891614c
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-05-20 23:04:39 +0300

    added getComments sql method

--- src/features/blog-content/comments/infrastructure/mongoose/comments.query-repository.ts
+++ src/features/blog-content/comments/infrastructure/mongoose/comments.query-repository.ts
@@ -59,13 +59,53 @@ export class CommentsQueryRepository {
       id,
       content,
       commentatorInfo: {
-        userId: commentator_id,
+        userId: commentator_id.toString(),
         userLogin: commentator_login,
       },
       createdAt: created_at,
       likesInfo,
     };
   }
+
+  // async getComments(
+  //   postId: string,
+  //   userId: string,
+  //   pagingParams: PagingParamsType,
+  // ): Promise<CommentsPaginatedType> {
+  //   const { sortBy, sortDirection, pageNumber, pageSize } = pagingParams;
+
+  //   const totalCount = await this.model.countDocuments({ postId });
+  //   const pagesCount = Math.ceil(totalCount / pageSize);
+
+  //   const commentsWithObjectId = await this.model
+  //     .find({ postId }, { postId: 0 })
+  //     .sort({ [sortBy]: sortDirection === 'asc' ? 1 : -1 })
+  //     .skip((pageNumber - 1) * pageSize)
+  //     .limit(pageSize)
+  //     .lean();
+
+  //   // const commentIds = commentsWithObjectId.map((comment) => comment._id.toString());
+
+  //   const comments = await Promise.all(
+  //     commentsWithObjectId.map(async (comment) => {
+  //       return {
+  //         id: comment._id.toString(),
+  //         content: comment.content,
+  //         commentatorInfo: comment.commentatorInfo,
+  //         createdAt: comment.createdAt,
+  //         likesInfo: await this.commentLikesQueryRepository.getLikesInfo(comment._id.toString(), userId),
+  //       };
+  //     }),
+  //   );
+
+  //   return {
+  //     pagesCount,
+  //     page: pageNumber,
+  //     pageSize,
+  //     totalCount,
+  //     items: comments,
+  //   };
+  // }
 
   async getComments(
     postId: string,
@@ -74,26 +114,62 @@ export class CommentsQueryRepository {
   ): Promise<CommentsPaginatedType> {
     const { sortBy, sortDirection, pageNumber, pageSize } = pagingParams;
 
-    const totalCount = await this.model.countDocuments({ postId });
+    let orderBy: string;
+    switch (sortBy) {
+      case 'commentatorInfo':
+        orderBy = 'commentator_id';
+        break;
+      case 'createdAt':
+        orderBy = 'created_at';
+        break;
+      default:
+        orderBy = sortBy;
+    }
+
+    const countResult = await this.pool.query(
+      `
+        SELECT id
+        FROM comments
+        WHERE post_id = $1
+      `,
+      [parseInt(postId)],
+    );
+
+    const totalCount = countResult.rowCount!;
     const pagesCount = Math.ceil(totalCount / pageSize);
+    const skipCount = (pageNumber - 1) * pageSize;
 
-    const commentsWithObjectId = await this.model
-      .find({ postId }, { postId: 0 })
-      .sort({ [sortBy]: sortDirection === 'asc' ? 1 : -1 })
-      .skip((pageNumber - 1) * pageSize)
-      .limit(pageSize)
-      .lean();
+    const commentsResult = await this.pool.query(
+      `
+        SELECT
+          comments.id
+          comments.content,
+          comments.created_at,
+          comments.user_id AS commentator_id,
+          users.login AS commentator_login
+        FROM comments JOIN users
+          ON comments.user_id = users.id
+        WHERE comments.post_id = $1
+        ORDER BY ${orderBy} ${sortDirection}
+        LIMIT $2
+        OFFSET $3
+      `,
+      [parseInt(postId), pageSize, skipCount],
+    );
 
-    // const commentIds = commentsWithObjectId.map((comment) => comment._id.toString());
+    const rawComments = commentsResult.rows;
 
     const comments = await Promise.all(
-      commentsWithObjectId.map(async (comment) => {
+      rawComments.map(async (comment) => {
         return {
-          id: comment._id.toString(),
+          id: comment.id.toString(),
           content: comment.content,
-          commentatorInfo: comment.commentatorInfo,
-          createdAt: comment.createdAt,
-          likesInfo: await this.commentLikesQueryRepository.getLikesInfo(comment._id.toString(), userId),
+          commentatorInfo: {
+            userId: comment.commentator_id.toString(),
+            userLogin: comment.commentator_login,
+          },
+          createdAt: comment.created_at,
+          likesInfo: await this.commentLikesQueryRepository.getLikesInfo(comment.id.toString(), userId),
         };
       }),
     );
