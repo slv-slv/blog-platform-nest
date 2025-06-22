2f9b9ea7a45f4362c96ee9112f8536d7d91f3288
Viacheslav Solovev <slvsl.spb@gmail.com>
2025-06-22 15:57:14 +0300

    added isSessionActive typeorm method

--- src/features/user-accounts/sessions/repositories/postgresql/sessions.query-repository.ts
+++ src/features/user-accounts/sessions/repositories/postgresql/sessions.query-repository.ts
@@ -2,22 +2,32 @@ import { Inject, Injectable } from '@nestjs/common';
 import { DeviceViewType } from '../../types/sessions.types.js';
 import { pool } from '../../../../../common/constants.js';
 import { Pool } from 'pg';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Device } from '../typeorm/sessions.entities.js';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class SessionsQueryRepository {
-  constructor(@Inject(pool) private readonly pool: Pool) {}
+  constructor(
+    @Inject(pool) private readonly pool: Pool,
+    @InjectRepository(Device) private readonly sessionEntityRepo: Repository<Device>,
+  ) {}
+
+  // async isSessionActive(userId: string, deviceId: string, iat: number): Promise<boolean> {
+  //   const result = await this.pool.query(
+  //     `
+  //       SELECT *
+  //       FROM devices
+  //       WHERE id = $1 AND user_id = $2 AND iat = $3
+  //     `,
+  //     [deviceId, parseInt(userId), iat],
+  //   );
+
+  //   return result.rowCount! > 0;
+  // }
 
   async isSessionActive(userId: string, deviceId: string, iat: number): Promise<boolean> {
-    const result = await this.pool.query(
-      `
-        SELECT *
-        FROM devices
-        WHERE id = $1 AND user_id = $2 AND iat = $3
-      `,
-      [deviceId, parseInt(userId), iat],
-    );
-
-    return result.rowCount! > 0;
+    return await this.sessionEntityRepo.existsBy({ id: deviceId, userId: parseInt(userId), iat });
   }
 
   async getActiveDevices(userId: string): Promise<DeviceViewType[]> {
