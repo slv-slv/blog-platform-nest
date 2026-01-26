853b36ba050cc92387ea18243fa934b01b7ab7b1
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-01-26 14:33:00 +0300

    small fixes: DomainExceptionFilter

--- src/common/exception-filters/domain-exception-filter.ts
+++ src/common/exception-filters/domain-exception-filter.ts
@@ -1,11 +1,19 @@
-import { Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
 import { DomainException } from '../exceptions/domain-exceptions.js';
 
 @Catch(DomainException)
 export class DomainExceptionFilter extends BaseExceptionFilter {
   catch(domainException: DomainException, host: ArgumentsHost) {
-    const httpException = new HttpException(domainException.message, domainException.getHttpCode());
+    const httpCode = domainException.getHttpCode();
+    const response =
+      httpCode === HttpStatus.BAD_REQUEST
+        ? {
+            errorsMessages: [{ message: domainException.message, field: domainException.field ?? 'unknown' }],
+          }
+        : domainException.message;
+
+    const httpException = new HttpException(response, httpCode);
     super.catch(httpException, host);
   }
 }
