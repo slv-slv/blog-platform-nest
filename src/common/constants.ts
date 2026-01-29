8e8ab6d223786d4a9cad6e78cd55e8492c2296a2
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-01-30 02:21:50 +0300

    added configModule constant

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,3 +1,14 @@
+import { ConfigModule } from '@nestjs/config';
+
 export const pgClient = 'pgClient';
 export const PG_POOL = 'PG_POOL';
 export const PG_BASE_CONFIG = 'PG_BASE_CONFIG';
+export const configModule = ConfigModule.forRoot({
+  envFilePath: [
+    // process.env.ENV_FILE_PATH,
+    `.env.development.local`,
+    `.env.${process.env.NODE_ENV}.local`,
+    `.env.${process.env.NODE_ENV}`,
+    `.env.production`,
+  ],
+});
