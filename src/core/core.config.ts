8a9e164778137cf75dfb14d54cffd4373bd1913e
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-02-02 23:04:39 +0300

    moved some settings from CoreConfig to DatabaseConfig, AuthConfig, EmailConfig

--- src/core/core.config.ts
+++ src/core/core.config.ts
@@ -4,22 +4,40 @@ import { PagingParamsType, SortDirection } from '../common/types/paging-params.t
 
 @Injectable()
 export class CoreConfig {
-  port2: number = this.configService.getOrThrow<number>('PORT');
-  constructor(private readonly configService: ConfigService) {}
+  readonly port: number;
+  readonly pagingDefaultParams: PagingParamsType;
+  readonly newestLikesNumber: number;
 
-  get port(): number {
-    return this.configService.getOrThrow<number>('PORT');
+  constructor(private readonly configService: ConfigService) {
+    this.port = this.configService.getOrThrow<number>('PORT');
+    this.pagingDefaultParams = {
+      sortBy: 'createdAt',
+      sortDirection: SortDirection.desc,
+      pageNumber: 1,
+      pageSize: 10,
+    };
+    this.newestLikesNumber = 3;
   }
+}
 
-  get mongoSettings() {
-    return {
+@Injectable()
+export class DatabaseConfig {
+  readonly mongoSettings: { url: string; database: string };
+  readonly postgresSettings: {
+    connectionString: string;
+    url: string;
+    user: string;
+    password: string;
+    database: string;
+    port: number;
+  };
+
+  constructor(private readonly configService: ConfigService) {
+    this.mongoSettings = {
       url: this.configService.getOrThrow<string>('MONGO_URL'),
       database: this.configService.getOrThrow<string>('MONGO_DATABASE'),
     };
-  }
-
-  get postgresSettings() {
-    return {
+    this.postgresSettings = {
       connectionString: this.configService.getOrThrow<string>('POSTGRES_CONNECTION_STRING'),
       url: this.configService.getOrThrow<string>('POSTGRES_URL'),
       user: this.configService.getOrThrow<string>('POSTGRES_USER'),
@@ -28,52 +46,37 @@ export class CoreConfig {
       port: 5432,
     };
   }
+}
 
-  get pagingDefaultParams(): PagingParamsType {
-    return {
-      sortBy: 'createdAt',
-      sortDirection: SortDirection.desc,
-      pageNumber: 1,
-      pageSize: 10,
-    };
+@Injectable()
+export class AuthConfig {
+  readonly adminCredentialsBase64: string;
+  readonly jwtPrivateKey: string;
+  readonly accessTokenLifetime: string;
+  readonly refreshTokenLifetime: string;
+  readonly confirmationCodeLifetime: number;
+  readonly recoveryCodeLifetime: number;
+
+  constructor(private readonly configService: ConfigService) {
+    this.adminCredentialsBase64 = this.configService.getOrThrow<string>('ADMIN_CREDENTIALS_BASE64');
+    this.jwtPrivateKey = this.configService.getOrThrow<string>('JWT_PRIVATE_KEY');
+    this.accessTokenLifetime = '10m';
+    this.refreshTokenLifetime = '20m';
+    this.confirmationCodeLifetime = 24;
+    this.recoveryCodeLifetime = 24;
   }
+}
 
-  get adminCredentialsBase64(): string {
-    return this.configService.getOrThrow<string>('ADMIN_CREDENTIALS_BASE64');
-  }
+@Injectable()
+export class EmailConfig {
+  readonly emailCredentials: { user: string; password: string };
+  readonly smtpUrl: string;
 
-  get emailCredentials() {
-    return {
+  constructor(private readonly configService: ConfigService) {
+    this.emailCredentials = {
       user: this.configService.getOrThrow<string>('EMAIL_LOGIN_GOOGLE'),
       password: this.configService.getOrThrow<string>('EMAIL_PASSWORD_GOOGLE'),
     };
-  }
-
-  get smtpUrl(): string {
-    return this.configService.getOrThrow<string>('SMTP_URL');
-  }
-
-  get jwtPrivateKey(): string {
-    return this.configService.getOrThrow<string>('JWT_PRIVATE_KEY');
-  }
-
-  get accessTokenLifetime(): string {
-    return '10m';
-  }
-
-  get refreshTokenLifetime(): string {
-    return '20m';
-  }
-
-  get confirmationCodeLifetime(): number {
-    return 24;
-  }
-
-  get recoveryCodeLifetime(): number {
-    return 24;
-  }
-
-  get newestLikesNumber(): number {
-    return 3;
+    this.smtpUrl = this.configService.getOrThrow<string>('SMTP_URL');
   }
 }
