88fd961b059524927c50ba7ea5e8606f9c290e0a
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-02-03 19:34:28 +0300

    implemented mongo config validation

--- src/config/mongo.config.ts
+++ src/config/mongo.config.ts
@@ -1,6 +1,24 @@
 import { registerAs } from '@nestjs/config';
+import { plainToInstance } from 'class-transformer';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { validateOrThrow } from './validate-or-throw.js';
 
-export const mongoConfig = registerAs('mongo', () => ({
-  url: process.env.MONGO_URL,
-  database: process.env.MONGO_DATABASE,
-}));
+class MongoConfigSchema {
+  @IsString()
+  @IsNotEmpty()
+  declare url: string;
+
+  @IsString()
+  @IsNotEmpty()
+  declare database: string;
+}
+
+export const mongoConfig = registerAs('mongo', () => {
+  const mongoConfigEnvInput = {
+    url: process.env.MONGO_URL,
+    database: process.env.MONGO_DATABASE,
+  };
+
+  const mongoConfigEnv = plainToInstance(MongoConfigSchema, mongoConfigEnvInput);
+  return validateOrThrow(mongoConfigEnv, 'mongo config');
+});
