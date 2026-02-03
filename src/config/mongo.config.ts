f8327c427baa87020393810b74a717551aee6d1a
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-02-03 16:19:55 +0300

    fixed string tokens of postgres and mongo namespaced configurations

--- src/config/mongo.config.ts
+++ src/config/mongo.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
-export const mongoConfig = registerAs('database', () => ({
+export const mongoConfig = registerAs('mongo', () => ({
   url: process.env.MONGO_URL,
   database: process.env.MONGO_DATABASE,
 }));
--- src/config/postgres.config.ts
+++ src/config/postgres.config.ts
@@ -1,6 +1,6 @@
 import { registerAs } from '@nestjs/config';
 
-export const postgresConfig = registerAs('database', () => ({
+export const postgresConfig = registerAs('postgres', () => ({
   connectionString: process.env.POSTGRES_CONNECTION_STRING,
   url: process.env.POSTGRES_URL,
   user: process.env.POSTGRES_USER,
