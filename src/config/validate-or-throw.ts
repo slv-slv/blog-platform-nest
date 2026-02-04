6d3c606ab359ed88673842d079a52156b45bece0
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-02-04 19:14:29 +0300

    added recursive error formattring

--- src/config/validate-or-throw.ts
+++ src/config/validate-or-throw.ts
@@ -1,10 +1,30 @@
-import { validateSync } from 'class-validator';
+import { ValidationError, validateSync } from 'class-validator';
 
 export function validateOrThrow<T extends object>(config: T, name = 'config'): T {
   const errors = validateSync(config, { whitelist: true, forbidNonWhitelisted: true });
   if (errors.length) {
-    throw new Error(`Invalid ${name}: ${JSON.stringify(errors)}`);
+    const formattedErrors = formatErrors(errors);
+    throw new Error(`Invalid ${name}: ${JSON.stringify(formattedErrors)}`);
   }
 
   return config;
+}
+
+type FormattedValidationError = {
+  property: string;
+  constraints?: ValidationError['constraints'];
+  children?: FormattedValidationError[];
+};
+
+function formatErrors(errors: ValidationError[]): FormattedValidationError[] {
+  return errors.map((error) => {
+    const property = error.property;
+    const constraints = error.constraints;
+
+    if (error.children?.length) {
+      return { property, constraints, children: formatErrors(error.children) };
+    }
+
+    return { property, constraints };
+  });
 }
