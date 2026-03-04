a31b254f0e7298fe70d45a23daf34584703866a1
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-03-04 23:03:12 +0300

    added e2e test: resend confirmation

---
+++ src/__tests__/security/auth/resend-confirmation.e2e.test.ts
@@ -0,0 +1,97 @@
+import { INestApplication } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+import request from 'supertest';
+import { App } from 'supertest/types.js';
+import { Pool } from 'pg';
+import { AppModule } from '../../../app.module.js';
+import { PG_POOL } from '../../../common/constants.js';
+import { authConfig } from '../../../config/auth.config.js';
+import { appSetup } from '../../../setup/app.setup.js';
+import { HTTP_STATUS } from '../../utils/http-status.js';
+import { EmailService } from '../../../notifications/email/email.service.js';
+
+describe('RESEND CONFIRMATION', () => {
+  let app: INestApplication<App>;
+  let httpServer: ReturnType<INestApplication<App>['getHttpServer']>;
+  let pool: Pool;
+  let adminAuthHeader = '';
+
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideProvider(EmailService)
+      .useValue({ sendConfirmationCode: () => {}, sendRecoveryCode: () => {} })
+      .compile();
+
+    app = moduleRef.createNestApplication();
+    appSetup(app);
+    await app.init();
+
+    httpServer = app.getHttpServer();
+    pool = app.get<Pool>(PG_POOL);
+    const auth = app.get<ConfigType<typeof authConfig>>(authConfig.KEY);
+    adminAuthHeader = `Basic ${auth.adminCredentialsBase64.replaceAll(`'`, '')}`;
+  });
+
+  beforeEach(async () => {
+    await request(httpServer).delete('/testing/all-data').expect(HTTP_STATUS.NO_CONTENT_204);
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should not resend code for not existing user', async () => {
+    await request(httpServer)
+      .post('/auth/registration-email-resending')
+      .send({ email: 'unknown@mail.io' })
+      .expect(HTTP_STATUS.BAD_REQUEST_400);
+  });
+
+  it('should not resend code for confirmed user', async () => {
+    await request(httpServer)
+      .post('/sa/users')
+      .set('Authorization', adminAuthHeader)
+      .send({
+        login: 'NewUser',
+        email: 'slvsl@vk.com',
+        password: 'somepassword',
+      })
+      .expect(HTTP_STATUS.CREATED_201);
+
+    await request(httpServer)
+      .post('/auth/registration-email-resending')
+      .send({ email: 'slvsl@vk.com' })
+      .expect(HTTP_STATUS.BAD_REQUEST_400);
+  });
+
+  it('should resend code for not confirmed user', async () => {
+    const user = { login: 'NewUser', email: 'slvsl@vk.com', password: 'somepassword' };
+    await request(httpServer).post('/auth/registration').send(user).expect(HTTP_STATUS.NO_CONTENT_204);
+
+    const beforeResendResult = await pool.query(`SELECT confirmation_code FROM users WHERE email = $1`, [
+      user.email,
+    ]);
+    const beforeResend = beforeResendResult.rows[0] ?? null;
+
+    await request(httpServer)
+      .post('/auth/registration-email-resending')
+      .send({ email: user.email })
+      .expect(HTTP_STATUS.NO_CONTENT_204);
+
+    const afterResendResult = await pool.query(
+      `
+        SELECT is_confirmed, confirmation_code
+        FROM users
+        WHERE email = $1
+      `,
+      [user.email],
+    );
+    const afterResend = afterResendResult.rows[0] ?? null;
+    expect(afterResend?.is_confirmed).toBe(false);
+    expect(afterResend?.confirmation_code).toBeTruthy();
+    expect(afterResend?.confirmation_code).not.toBe(beforeResend?.confirmation_code);
+  });
+});
