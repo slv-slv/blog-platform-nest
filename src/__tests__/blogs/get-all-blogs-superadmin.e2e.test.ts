f47aff75fea767c7d7f377a32d109c199349761f
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-03-06 23:19:39 +0300

    added e2e test: get all blogs (superadmin)

---
+++ src/__tests__/blogs/get-all-blogs-superadmin.e2e.test.ts
@@ -0,0 +1,72 @@
+import { INestApplication } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { Test, TestingModule } from '@nestjs/testing';
+import request from 'supertest';
+import { App } from 'supertest/types.js';
+import { AppModule } from '../../app.module.js';
+import { authConfig } from '../../config/auth.config.js';
+import { appSetup } from '../../setup/app.setup.js';
+import { HTTP_STATUS } from '../utils/http-status.js';
+import { EmailService } from '../../notifications/email/email.service.js';
+
+describe('GET ALL BLOGS SUPERADMIN', () => {
+  let app: INestApplication<App>;
+  let httpServer: ReturnType<INestApplication<App>['getHttpServer']>;
+  let adminAuthHeader = '';
+
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideProvider(EmailService)
+      .useValue({ sendConfirmationCode: () => {}, sendRecoveryCode: () => {} })
+      .compile();
+
+    app = moduleRef.createNestApplication();
+    appSetup(app);
+    await app.init();
+
+    const auth = app.get<ConfigType<typeof authConfig>>(authConfig.KEY);
+    adminAuthHeader = `Basic ${auth.adminCredentialsBase64.replaceAll(`'`, '')}`;
+    httpServer = app.getHttpServer();
+  });
+
+  beforeEach(async () => {
+    await request(httpServer).delete('/testing/all-data').expect(HTTP_STATUS.NO_CONTENT_204);
+  });
+
+  afterAll(async () => {
+    if (app) {
+      await app.close();
+    }
+  });
+
+  async function createBlog(name: string) {
+    await request(httpServer)
+      .post('/sa/blogs')
+      .set('Authorization', adminAuthHeader)
+      .send({
+        name,
+        description: `Description for ${name}`,
+        websiteUrl: `https://${name.toLowerCase()}.example.com`,
+      })
+      .expect(HTTP_STATUS.CREATED_201);
+  }
+
+  it('should return paginated blogs for superadmin', async () => {
+    await createBlog('BlogOne');
+    await createBlog('BlogTwo');
+
+    const response = await request(httpServer)
+      .get('/sa/blogs')
+      .set('Authorization', adminAuthHeader)
+      .expect(HTTP_STATUS.OK_200);
+
+    expect(response.body.totalCount).toBe(2);
+    expect(response.body.items).toHaveLength(2);
+  });
+
+  it('should return 401 without basic auth', async () => {
+    await request(httpServer).get('/sa/blogs').expect(HTTP_STATUS.UNAUTHORIZED_401);
+  });
+});
