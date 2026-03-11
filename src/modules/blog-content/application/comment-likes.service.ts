5c396ba512ae9b4bf67bf9d57d8969167bf38e8f
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-03-11 21:23:46 +0300

    deleted null check and domain exception throw

--- src/modules/blog-content/application/comment-likes.service.ts
+++ src/modules/blog-content/application/comment-likes.service.ts
@@ -3,7 +3,6 @@ import { CommentsRepository } from '../infrastructure/sql/comments.repository.js
 import { CommentLikesRepository } from '../infrastructure/sql/comment-likes.repository.js';
 import { CommentLikesType } from '../types/comment-likes.types.js';
 import { LikesInfoViewType, LikeStatus } from '../types/likes.types.js';
-import { CommentNotFoundDomainException } from '../../../common/exceptions/domain-exceptions.js';
 
 @Injectable()
 export class CommentLikesService {
@@ -13,8 +12,7 @@ export class CommentLikesService {
   ) {}
 
   async setLikeStatus(commentId: string, userId: string, likeStatus: LikeStatus): Promise<void> {
-    const comment = await this.commentsRepository.findComment(commentId);
-    if (!comment) throw new CommentNotFoundDomainException();
+    await this.commentsRepository.findComment(commentId);
 
     const currentLikeStatus = await this.commentLikesRepository.getLikeStatus(commentId, userId);
     if (likeStatus === currentLikeStatus) return;
