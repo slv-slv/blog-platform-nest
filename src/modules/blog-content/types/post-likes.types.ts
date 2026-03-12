61e74b2999f5aacc3561736297702d5c4326716f
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-03-12 18:19:32 +0300

    added param types for service and repo

--- src/modules/blog-content/types/post-likes.types.ts
+++ src/modules/blog-content/types/post-likes.types.ts
@@ -6,4 +6,21 @@ export type PostLikesType = {
   dislikes: { userId: string; createdAt: Date }[];
 };
 
+export type PostLikeStatusRepoParams = {
+  postId: string;
+  userId: string | null;
+};
+
+export type SetPostLikeRepoParams = {
+  postId: string;
+  userId: string;
+  createdAt: Date;
+};
+
+export type SetPostNoneRepoParams = {
+  postId: string;
+  userId: string;
+};
+
+// mongoose only
 export type PostLikesDbType = WithId<PostLikesType>;
