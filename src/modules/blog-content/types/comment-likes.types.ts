1ee67ed492f17d179764946275a99fa6319b78e4
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-03-12 18:20:22 +0300

    added param types for service and repo

--- src/modules/blog-content/types/comment-likes.types.ts
+++ src/modules/blog-content/types/comment-likes.types.ts
@@ -1,5 +1,5 @@
 import { WithId } from 'mongodb';
-import { LikeRecordType } from './likes.types.js';
+import { LikeRecordType } from '../infrastructure/mongoose/like-record-schema.js';
 
 export type CommentLikesType = {
   commentId: string;
@@ -7,4 +7,21 @@ export type CommentLikesType = {
   dislikes: LikeRecordType[];
 };
 
+export type CommentLikeStatusRepoParams = {
+  commentId: string;
+  userId: string | null;
+};
+
+export type SetCommentLikeRepoParams = {
+  commentId: string;
+  userId: string;
+  createdAt: Date;
+};
+
+export type SetCommentNoneRepoParams = {
+  commentId: string;
+  userId: string;
+};
+
+// mongoose only
 export type CommentLikesDbType = WithId<CommentLikesType>;
