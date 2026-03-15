b731b1e230740a5e71771e73bd61d0e7ae140cd0
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-03-15 21:04:45 +0300

    refactor: rewrite getLikesInfo to use batched queries

--- src/modules/blog-content/infrastructure/sql/comment-likes.query-repository.ts
+++ src/modules/blog-content/infrastructure/sql/comment-likes.query-repository.ts
@@ -1,15 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { CommentLikesRepository } from './comment-likes.repository.js';
-import { LikesInfoViewType } from '../../types/likes.types.js';
+import { LikeStatus, LikesInfoViewType } from '../../types/likes.types.js';
 
 @Injectable()
 export class CommentLikesQueryRepository {
   constructor(private readonly commentLikesRepository: CommentLikesRepository) {}
-  async getLikesInfo(commentId: string, userId: string | null): Promise<LikesInfoViewType> {
-    const likesCount = await this.commentLikesRepository.getLikesCount(commentId);
-    const dislikesCount = await this.commentLikesRepository.getDislikesCount(commentId);
-    const myStatus = await this.commentLikesRepository.getLikeStatus({ commentId, userId });
 
-    return { likesCount, dislikesCount, myStatus };
+  async getLikesInfo(commentIdArr: number[], userId: string | null): Promise<Map<number, LikesInfoViewType>> {
+    const likesCountArr = await this.commentLikesRepository.getLikesCount(commentIdArr);
+    const likesCountMap = new Map(
+      likesCountArr.map(({ commentId, likesCount }) => [commentId, likesCount]),
+    );
+
+    const dislikesCountArr = await this.commentLikesRepository.getDislikesCount(commentIdArr);
+    const dislikesCountMap = new Map(
+      dislikesCountArr.map(({ commentId, dislikesCount }) => [commentId, dislikesCount]),
+    );
+
+    const myStatusArr = await this.commentLikesRepository.getLikeStatus(commentIdArr, userId);
+    const myStatusMap = new Map(myStatusArr.map(({ commentId, myStatus }) => [commentId, myStatus]));
+
+    const likesInfoMap = new Map<number, LikesInfoViewType>();
+    for (const commentId of commentIdArr) {
+      likesInfoMap.set(commentId, {
+        likesCount: likesCountMap.get(commentId) ?? 0,
+        dislikesCount: dislikesCountMap.get(commentId) ?? 0,
+        myStatus: myStatusMap.get(commentId) ?? LikeStatus.None,
+      });
+    }
+
+    return likesInfoMap;
   }
 }
