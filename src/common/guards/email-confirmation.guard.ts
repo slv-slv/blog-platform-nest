14fddf9964cd093933664720b69afea5b97cf13c
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-03-30 16:47:10 +0300

    refactor: remove unneccesary DB request

--- src/common/guards/email-confirmation.guard.ts
+++ src/common/guards/email-confirmation.guard.ts
@@ -1,19 +1,15 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
-import { UsersService } from '../../modules/user-accounts/application/users.service.js';
 import { RequestWithUser } from '../types/requests.type.js';
 
 @Injectable()
 export class EmailConfirmationGuard implements CanActivate {
-  constructor(private readonly usersService: UsersService) {}
+  constructor() {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const req = context.switchToHttp().getRequest<RequestWithUser>();
-
     const user = req.user;
-    const email = user.email;
 
-    const isConfirmed = await this.usersService.isConfirmed(email);
-    if (!isConfirmed) {
+    if (!user.confirmation.isConfirmed) {
       throw new UnauthorizedException('Email has not been confirmed');
     }
 
