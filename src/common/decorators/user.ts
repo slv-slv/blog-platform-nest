2b95b3693cafe74772f27f914f1c945b06506d54
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-03-30 16:42:11 +0300

    refactor: add return type to decorators

--- src/common/decorators/user.ts
+++ src/common/decorators/user.ts
@@ -1,7 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { RequestWithUser } from '../types/requests.type.js';
+import { UserModel } from '../../modules/user-accounts/types/users.types.js';
 
-export const User = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+export const User = createParamDecorator((data: unknown, ctx: ExecutionContext): UserModel => {
   const request = ctx.switchToHttp().getRequest<RequestWithUser>();
   return request.user;
 });
--- src/common/decorators/userId.ts
+++ src/common/decorators/userId.ts
@@ -1,7 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { RequestWithUserId } from '../types/requests.type.js';
 
-export const UserId = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+export const UserId = createParamDecorator((data: unknown, ctx: ExecutionContext): string => {
   const request = ctx.switchToHttp().getRequest<RequestWithUserId>();
   return request.userId;
 });
