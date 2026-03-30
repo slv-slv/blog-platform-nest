d9ac7fcd7f0ca5cf78811503cb0cb153d7f73382
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-03-30 17:31:14 +0300

    refactor: add userId check for valid integer string

--- src/modules/user-accounts/infrastructure/typeorm/sessions.query-repository.ts
+++ src/modules/user-accounts/infrastructure/typeorm/sessions.query-repository.ts
@@ -3,12 +3,17 @@ import { DeviceViewModel } from '../../types/sessions.types.js';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Device } from './sessions.entities.js';
 import { Repository } from 'typeorm';
+import { isPositiveIntegerString } from '../../../../common/helpers/is-positive-integer-string.js';
 
 @Injectable()
 export class SessionsQueryRepository {
   constructor(@InjectRepository(Device) private readonly sessionEntityRepository: Repository<Device>) {}
 
   async getActiveDevices(userId: string): Promise<DeviceViewModel[]> {
+    if (!isPositiveIntegerString(userId)) {
+      return [];
+    }
+
     const devices = await this.sessionEntityRepository.findBy({ userId: +userId });
 
     return devices.map((device) => device.toViewModel());
