7f4de84febb1b23f4b47cb0df51e7d30b4ca939d
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-03-30 16:47:35 +0300

    refactor: remove unneccesary DB request

--- src/common/guards/credentials.guard.ts
+++ src/common/guards/credentials.guard.ts
@@ -8,16 +8,16 @@ import {
 import { plainToInstance } from 'class-transformer';
 import { validate } from 'class-validator';
 import { AuthService } from '../../modules/user-accounts/application/auth.service.js';
-import { LoginInputDto } from '../../modules/user-accounts/types/users.types.js';
-import { UsersQueryRepository } from '../../modules/user-accounts/infrastructure/sql/users.query-repository.js';
+import { LoginInputDto, UserModel } from '../../modules/user-accounts/types/users.types.js';
 import { RequestWithUser } from '../types/requests.type.js';
+import {
+  CredentialsIncorrectDomainException,
+  UserNotFoundDomainException,
+} from '../exceptions/domain-exceptions.js';
 
 @Injectable()
 export class CredentialsGuard implements CanActivate {
-  constructor(
-    private readonly authService: AuthService,
-    private readonly usersQueryRepository: UsersQueryRepository,
-  ) {}
+  constructor(private readonly authService: AuthService) {}
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const httpCtx = context.switchToHttp();
     const req = httpCtx.getRequest<RequestWithUser>();
@@ -30,14 +30,21 @@ export class CredentialsGuard implements CanActivate {
 
     const { loginOrEmail, password } = loginInputDto;
 
-    const isCorrect = await this.authService.checkCredentials(loginOrEmail, password);
-    if (!isCorrect) {
-      throw new UnauthorizedException('Incorrect login/password');
+    let user: UserModel;
+
+    try {
+      user = await this.authService.validateCredentials(loginOrEmail, password);
+    } catch (error) {
+      if (
+        error instanceof CredentialsIncorrectDomainException ||
+        error instanceof UserNotFoundDomainException
+      ) {
+        throw new UnauthorizedException('Incorrect login/password');
+      }
+      throw error;
     }
 
-    const user = await this.usersQueryRepository.findUser(loginOrEmail);
     req.user = user;
-
     return true;
   }
 }
