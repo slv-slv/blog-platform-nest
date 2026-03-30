de840f457e3a54b816426c6a69ce691f804b538c
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-03-30 16:41:51 +0300

    refactor: replace checkCredentials() with ValidateCredentials()

--- src/modules/user-accounts/application/auth.service.ts
+++ src/modules/user-accounts/application/auth.service.ts
@@ -4,7 +4,11 @@ import bcrypt from 'bcryptjs';
 import { UsersRepository } from '../infrastructure/sql/users.repository.js';
 import { JwtService } from '@nestjs/jwt';
 import { authConfig } from '../../../config/auth.config.js';
-import { CredentialsIncorrectDomainException } from '../../../common/exceptions/domain-exceptions.js';
+import {
+  CredentialsIncorrectDomainException,
+  UserNotFoundDomainException,
+} from '../../../common/exceptions/domain-exceptions.js';
+import { UserModel } from '../types/users.types.js';
 
 @Injectable()
 export class AuthService {
@@ -19,17 +23,18 @@ export class AuthService {
     return await bcrypt.hash(password, saltRounds);
   }
 
-  async checkCredentials(loginOrEmail: string, password: string): Promise<boolean> {
-    try {
-      const hash = await this.usersRepository.getPasswordHash(loginOrEmail);
-      return await bcrypt.compare(password, hash);
-    } catch (error) {
-      if (error instanceof CredentialsIncorrectDomainException) {
-        return false;
-      }
-
-      throw error;
+  async validateCredentials(loginOrEmail: string, password: string): Promise<UserModel> {
+    const user = await this.usersRepository.findUser(loginOrEmail);
+    if (!user) {
+      throw new UserNotFoundDomainException();
     }
+
+    const isCorrect = await bcrypt.compare(password, user.hash);
+    if (!isCorrect) {
+      throw new CredentialsIncorrectDomainException();
+    }
+
+    return user;
   }
 
   async generateAcessToken(userId: string): Promise<string> {
