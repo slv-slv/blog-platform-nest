db5e063ad2fdf4c3ecadfeb8dfe6585c645ea2d2
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-04-10 18:32:25 +0300

    feat: add getQuestions()

--- src/quiz-game/infrastructure/typeorm/questions.query-repository.ts
+++ src/quiz-game/infrastructure/typeorm/questions.query-repository.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Question } from './entities/question.entity.js';
+import { FindOptionsWhere, ILike, Repository } from 'typeorm';
+import {
+  GetQuestionsParams,
+  PublishedStatus,
+  QuestionsPaginatedViewModel,
+} from '../../types/question.types.js';
+import { mapQuestionToViewModel } from '../../mappers/question-view.mapper.js';
 
 @Injectable()
-export class QuestionsQueryRepository {}
+export class QuestionsQueryRepository {
+  constructor(@InjectRepository(Question) private readonly questionEntityRepository: Repository<Question>) {}
+
+  async getQuestions(params: GetQuestionsParams): Promise<QuestionsPaginatedViewModel> {
+    const { bodySearchTerm, publishedStatus, pagingParams } = params;
+    const { sortBy, sortDirection, pageNumber, pageSize } = pagingParams;
+
+    const where: FindOptionsWhere<Question> = {};
+
+    if (bodySearchTerm) {
+      where.body = ILike(`%${bodySearchTerm}%`);
+    }
+
+    const publishedMap: Record<PublishedStatus, boolean | null> = {
+      [PublishedStatus.published]: true,
+      [PublishedStatus.notPublished]: false,
+      [PublishedStatus.all]: null,
+    };
+
+    const published = publishedMap[publishedStatus];
+
+    if (published !== null) {
+      where.published = published;
+    }
+
+    const [questions, totalCount] = await this.questionEntityRepository.findAndCount({
+      where,
+      relations: { correctAnswers: true },
+      order: { [sortBy]: sortDirection },
+      take: pageSize,
+      skip: (pageNumber - 1) * pageSize,
+    });
+
+    return {
+      pagesCount: Math.ceil(totalCount / pageSize),
+      page: pageNumber,
+      pageSize,
+      totalCount,
+      items: questions.map((question) => mapQuestionToViewModel(question)),
+    };
+  }
+}
--- src/quiz-game/types/question.types.ts
+++ src/quiz-game/types/question.types.ts
@@ -1,3 +1,5 @@
+import { PagingParamsType } from '../../common/types/paging-params.types.js';
+
 export type QuestionViewModel = {
   id: string;
   body: string;
@@ -11,4 +13,30 @@ export type UpdateQuestionParams = {
   id: string;
   body: string;
   correctAnswers: string[];
+};
+
+export enum QuestionsSortBy {
+  body = 'body',
+  published = 'published',
+  createdAt = 'createdAt',
+}
+
+export enum PublishedStatus {
+  all = 'all',
+  published = 'published',
+  notPublished = 'notPublished',
+}
+
+export type GetQuestionsParams = {
+  bodySearchTerm: string | null;
+  publishedStatus: PublishedStatus;
+  pagingParams: PagingParamsType<QuestionsSortBy>;
+};
+
+export type QuestionsPaginatedViewModel = {
+  pagesCount: number;
+  page: number;
+  pageSize: number;
+  totalCount: number;
+  items: QuestionViewModel[];
 };
