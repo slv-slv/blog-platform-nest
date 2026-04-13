36fc8bb2a0754a4060e08aa30483fba910251940
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-04-13 16:11:31 +0300

    feat: add quiz config

---
+++ src/config/quiz.config.ts
@@ -0,0 +1,20 @@
+import { registerAs } from '@nestjs/config';
+import { plainToInstance, Type } from 'class-transformer';
+import { IsInt, Min } from 'class-validator';
+import { validateOrThrow } from './validate-or-throw.js';
+
+class QuizConfig {
+  @IsInt()
+  @Min(1)
+  @Type(() => Number)
+  declare questionsCount: number;
+}
+
+export const quizConfig = registerAs('quiz', () => {
+  const quizConfigEnvInput = {
+    questionsCount: process.env.QUIZ_QUESTIONS_COUNT,
+  };
+
+  const quizConfigEnv = plainToInstance(QuizConfig, quizConfigEnvInput);
+  return validateOrThrow(quizConfigEnv, 'quiz config');
+});
