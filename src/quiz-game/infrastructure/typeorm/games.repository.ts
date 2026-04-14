932dae9abf17813022f42f69b4bf38e6118687a9
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-04-14 19:10:31 +0300

    refactor: move some methods to other repositories

--- src/quiz-game/infrastructure/typeorm/games.repository.ts
+++ src/quiz-game/infrastructure/typeorm/games.repository.ts
@@ -1,21 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { Game } from './entities/game.entity.js';
-import { InjectDataSource, InjectRepository } from '@nestjs/typeorm';
-import { DataSource, EntityManager, Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { EntityManager, Repository } from 'typeorm';
 import { GameStatus } from '../../types/game.types.js';
-import { AnswerStatus, PlayerAnswer } from './entities/player-answer.entity.js';
-import { GameQuestion } from './entities/game-question.entity.js';
 import { isPositiveIntegerString } from '../../../common/helpers/is-positive-integer-string.js';
 import { UnauthorizedDomainException } from '../../../common/exceptions/domain-exceptions.js';
 
 @Injectable()
 export class GamesRepository {
-  constructor(
-    @InjectDataSource() private readonly dataSource: DataSource,
-    @InjectRepository(Game) private readonly gameEntityRepository: Repository<Game>,
-    @InjectRepository(GameQuestion) private readonly gameQuestionEntityRepository: Repository<GameQuestion>,
-    @InjectRepository(PlayerAnswer) private readonly playerAnswerEntityRepository: Repository<PlayerAnswer>,
-  ) {}
+  constructor(@InjectRepository(Game) private readonly gameEntityRepository: Repository<Game>) {}
 
   async save(game: Game, manager?: EntityManager): Promise<Game> {
     const gameEntityRepository = manager?.getRepository(Game) ?? this.gameEntityRepository;
@@ -59,56 +52,5 @@ export class GamesRepository {
       where: { status: GameStatus.pending },
       lock: { mode: 'pessimistic_write' },
     });
-  }
-
-  async getNextQuestion(gameId: string, userId: string): Promise<GameQuestion> {
-    const nextQuestion = await this.gameQuestionEntityRepository
-      .createQueryBuilder('gq')
-      .leftJoin(
-        PlayerAnswer,
-        'pa',
-        `gq."gameId" = pa."gameId"
-        AND gq."questionId" = pa."questionId"
-        AND pa."userId" = :userId`,
-        { userId: +userId },
-      )
-      .leftJoinAndSelect('gq.question', 'q')
-      .leftJoinAndSelect('q.correctAnswers', 'ca')
-      .where('gq.gameId = :gameId', { gameId: +gameId })
-      .andWhere('pa.questionId IS NULL')
-      .orderBy('gq.questionNumber', 'ASC')
-      .getOne();
-
-    if (!nextQuestion) {
-      throw new UnauthorizedDomainException();
-    }
-
-    return nextQuestion;
-  }
-
-  async submitAnswer(
-    gameId: string,
-    userId: string,
-    questionId: string,
-    answer: string,
-    status: AnswerStatus,
-  ): Promise<PlayerAnswer> {
-    await this.playerAnswerEntityRepository.insert({
-      gameId: +gameId,
-      questionId: +questionId,
-      userId: +userId,
-      answer,
-      status,
-    });
-
-    const newAnswer = this.playerAnswerEntityRepository.create({
-      gameId: +gameId,
-      questionId: +questionId,
-      userId: +userId,
-      answer,
-      status,
-    });
-
-    return this.playerAnswerEntityRepository.save(newAnswer);
   }
 }
