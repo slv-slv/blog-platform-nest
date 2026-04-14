1d9232abc10e9f855104f1a99b657993f44d0286
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-04-14 17:47:23 +0300

    feat: add PlayerAnswerInputDto

--- src/quiz-game/types/player-answer.types.ts
+++ src/quiz-game/types/player-answer.types.ts
@@ -1,3 +1,13 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+import { Trim } from '../../common/decorators/trim.js';
+
+export class PlayerAnswerInputDto {
+  @IsString()
+  @Trim()
+  @IsNotEmpty()
+  declare answer: string;
+}
+
 export type PlayerAnswerViewModel = {
   questionId: string;
   answerStatus: 'Correct' | 'Incorrect';
