6bd63c95c39171660f2bc77bbfe9379a80bad7d0
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-04-15 20:34:03 +0300

    feat: add connectUser()

--- src/quiz-game/api/pairs.controller.ts
+++ src/quiz-game/api/pairs.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
-import { QueryBus } from '@nestjs/cqrs';
+import { Controller, Get, HttpCode, Param, Post, UseGuards } from '@nestjs/common';
+import { CommandBus, QueryBus } from '@nestjs/cqrs';
 import { UserId } from '../../common/decorators/userId.js';
 import { AccessTokenGuard } from '../../common/guards/access-token.guard.js';
+import { ConnectUserCommand } from '../application/use-cases/connect-user.use-case.js';
 import { GetCurrentGameQuery } from '../application/use-cases/get-current-game.use-case.js';
 import { GetGameByIdQuery } from '../application/use-cases/get-game-by-id.use-case.js';
 import { GameViewModel } from '../types/game.types.js';
@@ -9,7 +10,10 @@ import { GameViewModel } from '../types/game.types.js';
 @Controller('pair-game-quiz/pairs')
 @UseGuards(AccessTokenGuard)
 export class PairsController {
-  constructor(private readonly queryBus: QueryBus) {}
+  constructor(
+    private readonly commandBus: CommandBus,
+    private readonly queryBus: QueryBus,
+  ) {}
 
   @Get('my-current')
   async getCurrentGame(@UserId() userId: string): Promise<GameViewModel> {
@@ -19,5 +23,11 @@ export class PairsController {
   @Get(':id')
   async getGameById(@Param('id') id: string, @UserId() userId: string): Promise<GameViewModel> {
     return this.queryBus.execute(new GetGameByIdQuery(id, userId));
+  }
+
+  @Post('connection')
+  @HttpCode(200)
+  async connectUser(@UserId() userId: string): Promise<GameViewModel> {
+    return this.commandBus.execute(new ConnectUserCommand(userId));
   }
 }
