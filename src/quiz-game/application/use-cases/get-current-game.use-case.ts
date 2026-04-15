64aa2eecd599c96ae802c612133000ff3924fdf1
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-04-15 20:00:04 +0300

    feat: add GetCurrentGameUseCase

---
+++ src/quiz-game/application/use-cases/get-current-game.use-case.ts
@@ -0,0 +1,18 @@
+import { IQueryHandler, Query, QueryHandler } from '@nestjs/cqrs';
+import { GamesQueryRepository } from '../../infrastructure/typeorm/games.query-repository.js';
+import { GameViewModel } from '../../types/game.types.js';
+
+export class GetCurrentGameQuery extends Query<GameViewModel> {
+  constructor(public readonly userId: string) {
+    super();
+  }
+}
+
+@QueryHandler(GetCurrentGameQuery)
+export class GetCurrentGameUseCase implements IQueryHandler<GetCurrentGameQuery> {
+  constructor(private readonly gamesQueryRepository: GamesQueryRepository) {}
+
+  async execute(query: GetCurrentGameQuery): Promise<GameViewModel> {
+    return this.gamesQueryRepository.getCurrentGameViewModel(query.userId);
+  }
+}
