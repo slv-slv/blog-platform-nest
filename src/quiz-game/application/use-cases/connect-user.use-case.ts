feef4b3bda2101aef627c36d834426e3edbe65cd
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-04-15 19:35:02 +0300

    fix: ConnectUserUseCase fixes

--- src/quiz-game/application/use-cases/connect-user.use-case.ts
+++ src/quiz-game/application/use-cases/connect-user.use-case.ts
@@ -2,7 +2,6 @@ import { Command, CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { InjectDataSource } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { GamesRepository } from '../../infrastructure/typeorm/games.repository.js';
-import { Game } from '../../infrastructure/typeorm/entities/game.entity.js';
 import { QuestionsRepository } from '../../infrastructure/typeorm/questions.repository.js';
 import { Inject } from '@nestjs/common';
 import { quizConfig } from '../../../config/quiz.config.js';
@@ -12,8 +11,10 @@ import {
   AccessDeniedDomainException,
   UnauthorizedDomainException,
 } from '../../../common/exceptions/domain-exceptions.js';
+import { GameViewModel } from '../../types/game.types.js';
+import { GamesQueryRepository } from '../../infrastructure/typeorm/games.query-repository.js';
 
-export class ConnectUserCommand extends Command<Game> {
+export class ConnectUserCommand extends Command<GameViewModel> {
   constructor(public readonly userId: string) {
     super();
   }
@@ -24,6 +25,7 @@ export class ConnectUserUseCase implements ICommandHandler<ConnectUserCommand> {
   constructor(
     @InjectDataSource() private readonly dataSource: DataSource,
     private readonly gamesRepository: GamesRepository,
+    private readonly gamesQueryRepository: GamesQueryRepository,
     private readonly questionsRepository: QuestionsRepository,
     @Inject(quizConfig.KEY) private readonly quiz: ConfigType<typeof quizConfig>,
   ) {}
@@ -31,11 +33,10 @@ export class ConnectUserUseCase implements ICommandHandler<ConnectUserCommand> {
     if (!isPositiveIntegerString(command.userId)) {
       throw new UnauthorizedDomainException();
     }
-
     const userId = +command.userId;
 
-    return this.dataSource.transaction(async (manager) => {
-      const activeGame = await this.gamesRepository.findActiveGameByUserId(command.userId, manager);
+    const savedGame = await this.dataSource.transaction(async (manager) => {
+      const activeGame = await this.gamesRepository.findActiveGame(command.userId, manager);
       if (activeGame) {
         throw new AccessDeniedDomainException('Current user is already inside active pair');
       }
@@ -50,7 +51,10 @@ export class ConnectUserUseCase implements ICommandHandler<ConnectUserCommand> {
       const questions = await this.questionsRepository.getRandomQuestions(manager, requiredQuestionsCount);
 
       pendingGame.startGame(userId, questions, requiredQuestionsCount);
+
       return this.gamesRepository.save(pendingGame, manager);
     });
+
+    return this.gamesQueryRepository.getGameViewModel(savedGame.id);
   }
 }
