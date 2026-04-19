73b029c1be76b72484738d01d652a92bebff80c3
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-04-19 15:01:31 +0300

    feat: add getMyStatistic()

--- src/modules/quiz-game/api/pairs.controller.ts
+++ src/modules/quiz-game/api/pairs.controller.ts
@@ -5,11 +5,12 @@ import { AccessTokenGuard } from '../../../common/guards/access-token.guard.js';
 import { ConnectUserCommand } from '../application/use-cases/connect-user.use-case.js';
 import { GetCurrentGameQuery } from '../application/use-cases/get-current-game.use-case.js';
 import { GetGameByIdQuery } from '../application/use-cases/get-game-by-id.use-case.js';
-import { GameViewModel, GetGameByIdParamDto } from '../types/game.types.js';
+import { GetMyStatisticQuery } from '../application/use-cases/get-my-statistic.use-case.js';
+import { GameViewModel, GetGameByIdParamDto, MyStatisticViewModel } from '../types/game.types.js';
 import { SubmitAnswerCommand } from '../application/use-cases/submit-answer.use-case.js';
 import { PlayerAnswerInputDto, PlayerAnswerViewModel } from '../types/player-answer.types.js';
 
-@Controller('pair-game-quiz/pairs')
+@Controller('pair-game-quiz')
 @UseGuards(AccessTokenGuard)
 export class PairsController {
   constructor(
@@ -17,24 +18,29 @@ export class PairsController {
     private readonly queryBus: QueryBus,
   ) {}
 
-  @Get('my-current')
+  @Get('users/my-statistic')
+  async getMyStatistic(@UserId() userId: string): Promise<MyStatisticViewModel> {
+    return this.queryBus.execute(new GetMyStatisticQuery(userId));
+  }
+
+  @Get('pairs/my-current')
   async getCurrentGame(@UserId() userId: string): Promise<GameViewModel> {
     return this.queryBus.execute(new GetCurrentGameQuery(userId));
   }
 
-  @Get(':id')
+  @Get('pairs/:id')
   async getGameById(@Param() params: GetGameByIdParamDto, @UserId() userId: string): Promise<GameViewModel> {
     const { id } = params;
     return this.queryBus.execute(new GetGameByIdQuery(id, userId));
   }
 
-  @Post('connection')
+  @Post('pairs/connection')
   @HttpCode(200)
   async connectUser(@UserId() userId: string): Promise<GameViewModel> {
     return this.commandBus.execute(new ConnectUserCommand(userId));
   }
 
-  @Post('my-current/answers')
+  @Post('pairs/my-current/answers')
   @HttpCode(200)
   async submitAnswer(
     @UserId() userId: string,
