1c4ee99bf9fe3e15c98af429ec8b196c92714e28
Viacheslav Solovev <slvsl.spb@gmail.com>
2026-04-20 21:02:15 +0300

    feat: add getTopPlayers()

--- src/modules/quiz-game/api/pairs.controller.ts
+++ src/modules/quiz-game/api/pairs.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, HttpCode, Param, Post, Query, UseGuards } from '@nestjs/common';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
+import { Public } from '../../../common/decorators/public.js';
 import { UserId } from '../../../common/decorators/userId.js';
 import { AccessTokenGuard } from '../../../common/guards/access-token.guard.js';
 import { ConnectUserCommand } from '../application/use-cases/connect-user.use-case.js';
@@ -7,12 +8,15 @@ import { GetCurrentGameQuery } from '../application/use-cases/get-current-game.u
 import { GetGameByIdQuery } from '../application/use-cases/get-game-by-id.use-case.js';
 import { GetMyGamesQuery } from '../application/use-cases/get-my-games.use-case.js';
 import { GetMyStatisticQuery } from '../application/use-cases/get-my-statistic.use-case.js';
+import { GetTopPlayersQuery } from '../application/use-cases/get-top-players.use-case.js';
 import {
   GamesPaginatedViewModel,
   GameViewModel,
   GetGameByIdParamDto,
   GetMyGamesQueryDto,
+  GetTopPlayersQueryDto,
   PlayerStatisticViewModel,
+  TopPlayersPaginatedViewModel,
 } from '../types/game.types.js';
 import { SubmitAnswerCommand } from '../application/use-cases/submit-answer.use-case.js';
 import { PlayerAnswerInputDto, PlayerAnswerViewModel } from '../types/player-answer.types.js';
@@ -24,6 +28,13 @@ export class PairsController {
     private readonly commandBus: CommandBus,
     private readonly queryBus: QueryBus,
   ) {}
+
+  @Get('users/top')
+  @Public()
+  async getTopPlayers(@Query() query: GetTopPlayersQueryDto): Promise<TopPlayersPaginatedViewModel> {
+    const { sort, pageNumber, pageSize } = query;
+    return this.queryBus.execute(new GetTopPlayersQuery({ sort, pageNumber, pageSize }));
+  }
 
   @Get('users/my-statistic')
   async getMyStatistic(@UserId() userId: string): Promise<PlayerStatisticViewModel> {
